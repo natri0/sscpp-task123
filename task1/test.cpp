05d4f6708d16e9dbc319752630c3b32bf1639aba
natri0 <dev@natri.fyi>
2025-09-12 13:51:58 +0300

    task1: tests for length, count, first_index_of

--- task1/test.cpp
+++ task1/test.cpp
@@ -54,3 +54,48 @@ TEST(StringList, remove_OnlyRemovesMatchingItems) {
     SL_remove(&list, str);
     ASSERT_EQ(1, SL_length(list)) << "SL_remove removed wrong item(s)";
 }
+
+TEST(StringList, length_ReturnsCorrectLength) {
+    const char *str = "Hello World";
+
+    auto list = SL_new();
+    ASSERT_EQ(0, SL_length(list)) << "SL_length doesn't think the list is empty";
+
+    SL_add(&list, str);
+    ASSERT_EQ(1, SL_length(list)) << "SL_length did not return correct length";
+}
+
+TEST(StringList, count_ReturnsCorrectCount) {
+    const char *str = "Hello World";
+    const char *str2 = "not Hello World";
+
+    auto list = SL_new();
+    ASSERT_EQ(0, SL_count(list, str)) << "SL_count found Hello World in an empty list";
+
+    SL_add(&list, str);
+    ASSERT_EQ(1, SL_count(list, str)) << "SL_count either did not find or found multiple of Hello World in the list";
+    ASSERT_EQ(0, SL_count(list, str2)) << "SL_count found not Hello World in a list that only has Hello World";
+
+    SL_add(&list, str2);
+    ASSERT_EQ(1, SL_count(list, str2));
+
+    SL_add(&list, str2);
+    ASSERT_EQ(2, SL_count(list, str2));
+}
+
+TEST(StringList, first_index_of_ReturnsMinus1IfNotFound) {
+    auto list = SL_new();
+    ASSERT_EQ(-1, SL_first_index_of(list, "Hello World")) << "SL_first_index_of found Hello World in an empty list";
+}
+
+TEST(StringList, first_index_of_ReturnsCorrectIndex) {
+    const char *str = "Hello World";
+
+    auto list = SL_new();
+    SL_add(&list, str);
+
+    ASSERT_EQ(0, SL_first_index_of(list, str)) << "SL_first_index_of did not find Hello World in a list that has it?";
+
+    SL_add(&list, str);
+    ASSERT_EQ(0, SL_first_index_of(list, str)) << "SL_first_index_of is supposed to return the _first_ index of this string :/";
+}
